ddaac7b6de38b6ebc31aed2f2269352b7048f67f
mthomas7503 <97054832+mthomas7503@users.noreply.github.com>
2022-04-19 18:00:04 -0400

    to show passing test

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution here
+function findMatching(arr, string) {return arr.filter(function (name) {return string.toUpperCase() == name.toUpperCase()})}
+
+function fuzzyMatch(arr, string) {return arr.filter(x => string[0]==x[0])}
+
+function matchName(arr, string) {return arr.filter(x => x.name === string)}
